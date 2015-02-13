03f5b64e7dde74850f66790c41cacf1ce8bb00c0
Marcus Trey <tmlibutan@gmail.com>
2015-02-13 09:26:06 -0800

    First Commit

--- HW15/src/SavingsAccount.java
+++ HW15/src/SavingsAccount.java
@@ -1,4 +1,34 @@
+public class SavingsAccount extends Account {
+	double interestRate = 0.03;
 
-public class SavingsAccount {
+	public SavingsAccount(double bal, int accNum) {
+		super(bal, accNum);
+	}
 
+	public double computeMonthlySavingsBalance() {
+		return this.balance + ((this.balance * this.interestRate) / 12);
+
+	}
+
+	public String getAccountType() {
+		return "SAVINGS";
+	}
+
+	public static void main(String[] args) {
+		SavingsAccount account = new SavingsAccount(2000, 43);
+		if (account.getAccountType().equals("SAVINGS")) {
+			System.out.println(true);
+			
+		} else {
+			System.out.println(false);
+			
+		}
+		if (account.computeMonthlySavingsBalance() == 2005) {
+			System.out.println(true);
+			
+		} else {
+			System.out.println(false);
+			
+		}
+	}
 }
